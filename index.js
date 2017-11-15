e0090315ae109008d47a747fab59a02c0dd76dd9
Erica Frank <ericarfrank@gmail.com>
2017-11-15 19:19:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase()) {
-    return "I can\'t hear you!"
-  } else if(string === string.toUpperCase()) {
-    return "YES INDEED!"
-  } else if (string = "I love you, grandma") {
-    return "I love you, too"
+  if(string.toLowerCase() === "i love you, grandma.") {
+    return "I love you, too.";
+  } else if(string.toLowerCase() === string) {
+    return "I can't hear you!";
+  } else if (string.toUpperCase() === string) {
+    return "YES INDEED!";
   }
 }
