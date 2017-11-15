535205ec262b230e9034cf53389c50896fe5e2fe
Erica Frank <ericarfrank@gmail.com>
2017-11-15 19:02:51 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (sayHiToGrandma = string.toLowerCase()) {
+    return "I can't hear you!"
+  }
+
+  function sayHiToGrandma(string) {
+  if (sayHiToGrandma = string.toUpperCase()) {
+    return "YES INDEED!"
+  } else if (sayHiToGrandma = "I love you, grandma") {
+    return "I love you, too"
+  }
+}
